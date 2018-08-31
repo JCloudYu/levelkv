5785c051e5849f94a7febd26066235707a067779
JCloudYu <j.cloud.yu@gmail.com>
2018-08-31 16:10:08 +0800

    Add db declarations

---
+++ levelkv-bson.js
@@ -0,0 +1,28 @@
+/**
+ * Project: levelkv
+ * File: levelkv-bson
+ * Author: JCloudYu
+ * Create Date: Aug. 31, 2018 
+ */
+(()=>{
+	"use strict";
+	
+	class LevelKV_DB_BSON {
+		constructor(dir, options={}){
+		
+		}
+		async close() {
+		
+		}
+		get() {
+		
+		}
+		async put() {
+		
+		}
+		async del() {
+		
+		}
+	}
+	module.exports=LevelKV_DB_BSON;
+})();
